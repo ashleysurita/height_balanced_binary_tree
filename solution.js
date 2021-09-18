2501505a2ee8cafffa109c11b4360832b961ba5a
Ashley <ashleywong678@gmail.com>
2021-09-18 19:45:45 -0400

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,32 @@
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class TreeInfo {
+	constructor(isBalanced, height) {
+		this.isBalanced = isBalanced
+		this.height = height
+	}
+}
+ 
+function heightBalancedBinaryTree(tree) {
+  const treeInfo = getTreeInfo(tree)
+	return treeInfo.isBalanced
+}
+
+function getTreeInfo(node) {
+	if(!node) return new TreeInfo(true, -1)
+	
+	const left = getTreeInfo(node.left)
+	const right = getTreeInfo(node.right)
+	
+	const isBalanced = left.isBalanced && right.isBalanced && Math.abs(left.height - right.height) <= 1
+	
+	const height = Math.max(left.height, right.height) + 1
+	
+	return new TreeInfo(isBalanced, height)
+}
